8b9a914c8505d83de03aa2d99df0481104cfee0e
hades42 <nguyenvannguyen.oc@gmail.com>
2020-07-29 10:23:15 +1000

    Update Main.java
    add some variable

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World !!!");
         String symbol = "*";
+        String hello = " Heeloo ";
 
     }
 }
